4042a900d3e7c0bd01627e8a1b9783bb20326839
unknown <melanieleung@ucsb.edu>
2021-05-12 11:46:04 -0700

    plotting more rasters

--- tutorial-2.R
+++ tutorial-2.R
@@ -29,8 +29,22 @@ ggplot() +
   geom_raster(data = DSM_HARV_df, aes(x=x, y=y, fill=fct_elevation_2)) +
   coord_quickmap()
 
-# plot with nicer colors
+# plot with nicer colors and additional fun stuff like axis formatting
 terrain.colors(3)
 ggplot() +
-  geom_raster(data = DSM_HARV_df, aes(x=x, y=y, fill=fct_elevation_2, ))
+  geom_raster(data = DSM_HARV_df, aes(x=x, y=y, fill=fct_elevation_2, )) +
+  scale_fill_manual(values = terrain.colors(3), name = "Elevation") +
+  theme(axis.title = element_blank()) +
+  coord_quickmap()
+
+### add a hillshade (via overlaying)
+DSM_hill_HARV <- raster("C:/Users/collabuser/Desktop/geospatialWorkshop/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/HARV_DSMhill.tif")
+DSM_hill_HARV_df <- as.data.frame(DSM_hill_HARV, xy=T)
+str(DSM_hill_HARV_df)
+
+ggplot() +
+  geom_raster(data = DSM_hill_HARV_df,
+              aes(x=x, y=y, alpha = HARV_DSMhill)) +
+  scale_alpha(range = c(0.15, 0.65))
+# make sure crs is the same before overlaying
 
